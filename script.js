4e3b5577221bf4423b65cab1e944bd6e5070c380
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-09-20 14:32:32 +0000

    minor changes

--- script.js
+++ script.js
@@ -45,6 +45,18 @@ if(savedUsername === null){ // for first time submission
 
 */
 
-const loginInput = loginForm.querySelector("#login-form input");
-const loginButton = loginForm.querySelector("#login-form button")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick(){
+  const username = loginInput.value;
+  if (username == ""){
+    alert("Please write your name.");
+  } else if(username.length > 15){
+    alert("Your name is too long.");
+  }
+
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
 
