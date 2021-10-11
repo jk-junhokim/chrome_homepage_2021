bb3f87817fafc946953424a85f854fb0f9062b41
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2021-10-11 10:56:24 +0000

    took care of clock. found amazing model for the challenge

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,17 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("div#clock");
+const clock_date = clock.querySelector("p.date");
+const clock_time = clock.querySelector("p.time");
+const WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+
 
 function getClock(){
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock_date.innerText= `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${WEEK[date.getDay()]}`;
+  clock_time.innerText = `${hours}:${minutes}:${seconds}`
 }
-
-// setInterval(sayHello, 5000);
-// setTimeout(sayHello, 5000);
 
 getClock();
 setInterval(getClock, 1000); //calls designated function every 1000 miliseconds
